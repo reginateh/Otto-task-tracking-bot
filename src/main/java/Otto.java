55b96945313ae5e6ae3240f35531b823501ab59b
reginateh <e1312290@u.nus.edu>
2024-08-22 15:19:46 +0800

    Otto.java: Add user input and echo

--- src/main/java/Otto.java
+++ src/main/java/Otto.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class Otto {
+    private static void intro() {
+        String owl = "\t            z\n"
+                + "\t          z\n"
+                + "\t   ^_^  z\n"
+                + "\t  (-,-)  \n"
+                + "\t  { \" }  \n"
+                + "\t---\"-\"---\n";
+        String intro = "\n\tOtto would rather be napping, \n\tbut he suppose he can help you with your tasks.\n";
+        System.out.println(owl + intro);
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println("\t____________________________________________________________\n");
+    }
+
+    private static void exit() {
+        printLine();
+        System.out.println("\tOtto is signing off now. Don’t wake him up again.");
+        printLine();
+    }
+
+    private static void displayMsg(String msg) {
+        printLine();
+        System.out.println("\t" + msg);
+        printLine();
+    }
+
     public static void main(String[] args) {
-        String owl = "            z\n"
-                + "          z\n"
-                + "   ^_^  z\n"
-                + "  (-,-)  \n"
-                + "  { \" }  \n"
-                + "---\"-\"---\n";
-        String intro = "Otto would rather be napping, \nbut he suppose he can help you with your tasks.\n";
-        String line = "____________________________________________________________\n";
-        String bye = "Otto is signing off now. Don’t wake him up again.\n";
-        System.out.println(owl + intro + line + bye + line);
+        intro();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine().toLowerCase();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            displayMsg(userInput);
+        }
+        exit();
     }
 }
