bea42fe43f80bc3dd0667ae7c8fe8ba6879db3f5
reginateh <e1312290@u.nus.edu>
2024-08-22 14:46:58 +0800

    Change Duke to Otto

---
+++ src/main/java/Otto.java
@@ -0,0 +1,14 @@
+public class Duke {
+    public static void main(String[] args) {
+        String owl = "            z\n"
+                + "          z\n"
+                + "   ^_^  z\n"
+                + "  (-,-)  \n"
+                + "  { \" }  \n"
+                + "---\"-\"---\n";
+        String intro = "Otto would rather be napping, \nbut he suppose he can help you with your tasks.\n";
+        String line = "____________________________________________________________\n";
+        String bye = "Otto is signing off now. Don’t wake him up again.\n";
+        System.out.println(owl + intro + line + bye + line);
+    }
+}
